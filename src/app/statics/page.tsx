f7de549ab51d4f4d98558b4fe137ab261722b62c
n0rmanc <s965202030@gmail.com>
2024-04-03 22:03:17 +0800

    add statics page

---
+++ src/app/statics/page.tsx
@@ -0,0 +1,15 @@
+export async function getData() {
+    return {
+        name: 'John Doe',
+    }
+  }
+
+export default async function Page() {
+    const resp  = await fetch('https://api.ipify.org?format=json', {cache: "default"})
+    const ip = await resp.text()
+  return (
+    <div>
+      <h1>{ip}</h1>
+    </div>
+  );
+}
