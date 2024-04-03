a00c7a96edc5cf6718eb80448b2e63fbd10a6dca
n0rmanc <s965202030@gmail.com>
2024-04-03 22:37:57 +0800

    Initial commit from create-refine-app

--- src/app/statics/page.tsx
+++ src/app/statics/page.tsx
@@ -1,12 +1,14 @@
 async function getData() {
-    return {
-        name: 'John Doe',
-    }
-  }
+  return {
+    name: "John Doe",
+  };
+}
 
 export default async function Page() {
-    const resp  = await fetch('https://api.ipify.org?format=json', {cache: "default"})
-    const ip = await resp.text()
+  const resp = await fetch("https://api.ipify.org?format=json", {
+    cache: "default",
+  });
+  const ip = await resp.text();
   return (
     <div>
       <h1>{ip}</h1>
